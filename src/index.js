1cefc26c32222df87b63051effb7aef15b0d8b8f
Kyle Chadwick <kchadwick@squiz.co.nz>
2016-12-24 03:08:21 +1300

    Added react redux routing & classnames plugins

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import store from './store'
+import { Router, Route, hashHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+const history = syncHistoryWithStore(hashHistory, store)
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
