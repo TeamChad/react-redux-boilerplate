999582407483ad25d7f8e9dcfc0681cab010ed85
Kyle Chadwick <kchadwick@squiz.co.nz>
2017-05-18 14:25:22 +1200

    Updated Redux Logger version

--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, createStore, compose } from "redux"
 import {responsiveStoreEnhancer} from 'redux-responsive'
-import createLogger from "redux-logger"
 import thunk from "redux-thunk"
+import logger from 'redux-logger'
 
 import reducer from "./reducers"
 
 const middleware = [thunk]
 
 if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
+  middleware.push(logger)
 }
 
 export default createStore(
